5f6e5804c72c8b8c19ed266993757ddc2fffc1fc
zaxwebs <zifidigital@gmail.com>
2024-09-19 09:46:42 +0530

    added heater calculator

--- src/lib/utils/calculators.js
+++ src/lib/utils/calculators.js
@@ -8,5 +8,15 @@ function calculateFiltrationFlowRate(tankVolume, turnoverRate = 4) {
 	return tankVolume * turnoverRate;
 }
 
+/**
+ * Calculates the recommended heater wattage for an aquarium.
+ * @param {number} tankSize - The size of the aquarium in gallons.
+ * @param {number} wattsPerGallon - The number of watts needed per gallon. Default is 4 watts per gallon.
+ * @returns {number} - The recommended heater wattage.
+ */
+function calculateHeaterWattage(tankVolume, wattsPerGallon = 4) {
+	return tankSize * wattsPerGallon;
+}
+
 // Export the function
 export { calculateFiltrationFlowRate };
